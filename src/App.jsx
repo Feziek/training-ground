63df9174eb7c6f31598e4a0874ce9740de7a3e4f
Feziek <josephclarksabado575@gmail.com>
2026-03-26 23:29:10 +0800

    feat: update app title

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import "./App.css";
+import { useState } from 'react';
+import './App.css';
 
 function App() {
 	const [count, setCount] = useState(0);
@@ -7,15 +7,13 @@ function App() {
 	return (
 		<>
 			<section id="center">
-				<h1>Get started</h1>
+				<h1>Counter</h1>
 				<button
 					className="counter"
-					onClick={() => setCount(count => count + 1)}
-				>
+					onClick={() => setCount(count => count + 1)}>
 					Count is {count}
 				</button>
 			</section>
-
 		</>
 	);
 }
