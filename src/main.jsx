34cc3017af676ed5c0991290937aa51a2ef5d756
jnelbaylen <jnelbaylen@gmail.com>
2026-03-31 22:58:32 +0800

    chore: update import

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
+import './styles/global.css'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+
+import { App } from './pages/App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
